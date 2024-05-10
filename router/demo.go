77e5fb547ceb24e4c9a5eda5f44d9d57f5a6b801
Quan <Hilda_quan@163.com>
2024-05-10 16:39:48 +0800

    feat: Implement simple dynamic routing framework

--- router/demo.go
+++ router/demo.go
@@ -6,7 +6,13 @@ import (
 	"github.com/zzzgydi/webscraper/router/middleware"
 )
 
-func DemoRouter(r *gin.Engine) {
+func init() {
+	RegisterRoute(func(r *gin.Engine) {
+		demoRouter(r)
+	})
+}
+
+func demoRouter(r *gin.Engine) {
 	r.LoadHTMLGlob("static/*")
 	r.GET("/", middleware.LoggerMiddleware, controller.GetScrapeHTML)
 }
